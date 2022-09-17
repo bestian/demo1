cf69af80a9e46526cf0a8406febcf89143ce2826
Bestian Tang 小巴 <bestian@gmail.com>
2022-09-17 14:24:18 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/demo1/'
+  : '/',
   transpileDependencies: true
 })
