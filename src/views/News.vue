75fc046ed1ea72233d753f229425bb322fa85ee5
Bestian Tang 小巴 <bestian@gmail.com>
2022-09-19 10:44:25 +0800

    small

--- src/views/News.vue
+++ src/views/News.vue
@@ -2,7 +2,7 @@
   <div class="about">
     <h1>最新消息</h1>
     <div class="ui ordered list">
-      <div class="item" v-for = "n in news" :key="n.date">{{n.date}}: {{n.n}}</div>
+      <div class="item" v-for = "n in news" :key="n.date">{{n.date}}: {{n.name}}</div>
     </div>
   </div>
 </template>
@@ -14,15 +14,15 @@ export default {
   data () {
     return {
       news: [
-        { n: 'Lorem Ipsum', date: '2022-09-17', href: '' },
-        { n: 'Lorem Ipsum', date: '2022-09-15', href: '' },
-        { n: 'Lorem Ipsum', date: '2022-09-12', href: '' },
-        { n: 'Lorem Ipsum', date: '2022-09-10', href: '' },
-        { n: 'Lorem Ipsum', date: '2022-09-05', href: '' },
-        { n: 'Lorem Ipsum', date: '2022-08-26', href: '' },
-        { n: 'Lorem Ipsum', date: '2022-08-17', href: '' },
-        { n: 'Lorem Ipsum', date: '2022-08-09', href: '' },
-        { n: 'Lorem Ipsum', date: '2022-07-27', href: '' }
+        { name: 'Lorem Ipsum', date: '2022-09-17', href: '' },
+        { name: 'Lorem Ipsum', date: '2022-09-15', href: '' },
+        { name: 'Lorem Ipsum', date: '2022-09-12', href: '' },
+        { name: 'Lorem Ipsum', date: '2022-09-10', href: '' },
+        { name: 'Lorem Ipsum', date: '2022-09-05', href: '' },
+        { name: 'Lorem Ipsum', date: '2022-08-26', href: '' },
+        { name: 'Lorem Ipsum', date: '2022-08-17', href: '' },
+        { name: 'Lorem Ipsum', date: '2022-08-09', href: '' },
+        { name: 'Lorem Ipsum', date: '2022-07-27', href: '' }
       ]
     }
   }
